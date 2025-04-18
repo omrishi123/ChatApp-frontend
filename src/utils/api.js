ca550c28cb5bd5e960efbd2be2aafa1aa9139f9a
omrishi123 <omrishi9608@gmail.com>
2025-04-18 23:20:14 +0530

    Refactor API to use Axios instance with environment base URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -30,52 +30,52 @@ export const updateProfile = async (token, data) => {
   Object.entries(data).forEach(([key, value]) => {
     if (value) form.append(key, value);
   });
-  const res = await API.put('/auth/me', form, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.put('/api/auth/me', form, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const searchUsers = async (token, username) => {
-  const res = await API.get(`/users/search?username=${username}`, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.get(`/api/users/search?username=${username}`, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const getUserProfile = async (token, id) => {
-  const res = await API.get(`/users/${id}`, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.get(`/api/users/${id}`, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const blockUser = async (token, userId) => {
-  const res = await API.post('/users/block', { userId }, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.post('/api/users/block', { userId }, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const unblockUser = async (token, userId) => {
-  const res = await API.post('/users/unblock', { userId }, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.post('/api/users/unblock', { userId }, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const getChats = async (token) => {
-  const res = await API.get('/chats', { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.get('/api/chats', { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const startChat = async (token, userId) => {
-  const res = await API.post('/chats/start', { userId }, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.post('/api/chats/start', { userId }, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const muteChat = async (token, chatId) => {
-  const res = await API.post('/chats/mute', { chatId }, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.post('/api/chats/mute', { chatId }, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const unmuteChat = async (token, chatId) => {
-  const res = await API.post('/chats/unmute', { chatId }, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.post('/api/chats/unmute', { chatId }, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const getMessages = async (token, chatId) => {
-  const res = await API.get(`/messages/${chatId}`, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.get(`/api/messages/${chatId}`, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
@@ -84,21 +84,21 @@ export const sendMessage = async (token, data) => {
   Object.entries(data).forEach(([key, value]) => {
     if (value) form.append(key, value);
   });
-  const res = await API.post('/messages/send', form, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.post('/api/messages/send', form, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const deleteMessage = async (token, messageId, forEveryone) => {
-  const res = await API.post('/messages/delete', { messageId, forEveryone }, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.post('/api/messages/delete', { messageId, forEveryone }, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const starMessage = async (token, messageId) => {
-  const res = await API.post('/messages/star', { messageId }, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.post('/api/messages/star', { messageId }, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
 
 export const unstarMessage = async (token, messageId) => {
-  const res = await API.post('/messages/unstar', { messageId }, { headers: { Authorization: `Bearer ${token}` } });
+  const res = await API.post('/api/messages/unstar', { messageId }, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
