abac84c18d763cdd47054801991b240c28c56c6d
omrishi123 <omrishi9608@gmail.com>
2025-04-28 14:05:07 +0530

    repair

---
+++ src/components/Chat.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import ChatHeader from './ChatHeader';
+import Messages from './Messages';
+import ChatFooter from './ChatFooter';
+import StatusTab from '../Status/StatusTab';
+
+const Chat = () => {
+    const [text, setText] = useState('');
+
+    return (
+        <Box>
+            <ChatHeader />
+            <Messages />
+            <ChatFooter text={text} setText={setText} />
+        </Box>
+    );
+};
+
+export default Chat;
