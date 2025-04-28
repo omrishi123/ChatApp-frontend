ea473cc9d2f4ccc357e070fac5a163bbea56b631
omrishi123 <omrishi9608@gmail.com>
2025-04-28 14:15:50 +0530

    remove status

--- src/Status/StatusTab.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const StatusTab = () => {
-    return <div>Status Tab Component</div>;
-};
-
-export default StatusTab;
--- src/components/Chat/StatusTab.js
+++
@@ -1,2 +0,0 @@
-// Proxy re-export to fix import errors after moving StatusTab to Status folder
-export { default } from '../Status/StatusTab';
